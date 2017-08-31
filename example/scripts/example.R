91347899ece7dec91332b09fa9978e8c8b0bef12
Rachel Schwartz <rsschwartz@gmail.com>
2017-08-31 11:52:06 -0400

    add script

--- example/scripts/example.R
+++ example/scripts/example.R
@@ -3,6 +3,7 @@ download.file("https://ndownloader.figshare.com/files/2292169",
               "data/portal_data_joined.csv")
 surveys <- read.csv("data/portal_data_joined.csv")
 
+#this is how I get info about the data
 head(surveys)
 dim(surveys)
 nrow(surveys)
@@ -10,3 +11,6 @@ ncol(surveys)
 names(surveys)
 head(rownames(surveys))
 str(surveys)
+
+#access some of the data
+all_sp_ids <- surveys$species_id
